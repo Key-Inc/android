43541a5073bbf55df8b70aaf80d2a14aea4e5c86
lantt <lanttoff@gmail.com>
2024-02-15 13:31:29 +0700

    add domain and data layers
    - added domain with Classroom entity
    - added data with a list of mock classrooms

---
+++ app/src/main/java/com/keyinc/keymono/data/MockClassrooms.kt
@@ -0,0 +1,15 @@
+package com.keyinc.keymono.data
+
+import com.keyinc.keymono.domain.entity.Classroom
+
+object MockClassrooms {
+    val classrooms = listOf(
+        Classroom(101),
+        Classroom(102),
+        Classroom(105),
+        Classroom(220),
+        Classroom(221),
+        Classroom(302),
+        Classroom(312),
+    )
+}
