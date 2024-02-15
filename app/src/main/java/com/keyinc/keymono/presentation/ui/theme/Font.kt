a560900b07405d6fc61d786e46998dd374971cdd
lantt <lanttoff@gmail.com>
2024-02-15 13:30:46 +0700

    move ui/theme to presentation
    - moved ui/theme to presentation
    - added fonts, textstyles and colors

---
+++ app/src/main/java/com/keyinc/keymono/presentation/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.keyinc.keymono.presentation.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.keyinc.keymono.R
+
+val Inter = FontFamily(
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_semibold, FontWeight.SemiBold)
+)
