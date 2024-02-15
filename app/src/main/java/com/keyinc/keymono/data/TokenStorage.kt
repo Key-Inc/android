1e4b933c786d9583240749bd3e9b10dbf45d151d
Yuriy Yangel <arlosaneq@gmail.com>
2024-02-15 18:41:44 +0800

    Added companion object for shared prefs const

--- app/src/main/java/com/keyinc/keymono/data/TokenStorage.kt
+++ app/src/main/java/com/keyinc/keymono/data/TokenStorage.kt
@@ -8,28 +8,34 @@ import javax.inject.Singleton
 @Singleton
 class TokenStorage(context: Context)  {
 
+    private companion object {
+        const val TOKEN_REFERENCE = "secret_shared_prefs"
+        const val SHARED_KEY = "token"
+    }
+
+
     private val masterKey: MasterKey =
         MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
 
 
     private val encryptedSharedPreferences =
         EncryptedSharedPreferences.create(
-            context, "secret_shared_prefs", masterKey,
+            context, TOKEN_REFERENCE, masterKey,
             EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
             EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
         )
 
     fun deleteToken() {
-        encryptedSharedPreferences.edit().remove("token").apply()
+        encryptedSharedPreferences.edit().remove(SHARED_KEY).apply()
     }
 
     fun getToken(): String {
-        return encryptedSharedPreferences.getString("token", "")
+        return encryptedSharedPreferences.getString(SHARED_KEY, "")
             ?: ""
     }
 
     fun saveToken(token: String) {
-        encryptedSharedPreferences.edit().putString("token", token).apply()
+        encryptedSharedPreferences.edit().putString(SHARED_KEY, token).apply()
     }
 
 }
