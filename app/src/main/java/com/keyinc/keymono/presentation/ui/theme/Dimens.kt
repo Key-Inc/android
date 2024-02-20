d40b094adf412fefd0386174051d16e6b572f31c
Matvey Seregin <daymatt@mail.ru>
2024-02-20 22:59:27 +0700

    add new theme values
    - added new dimen
    - reordered dimens
    - added new text style
    - added new strings resources for SendRequestScreen

--- app/src/main/java/com/keyinc/keymono/presentation/ui/theme/Dimens.kt
+++ app/src/main/java/com/keyinc/keymono/presentation/ui/theme/Dimens.kt
@@ -2,8 +2,10 @@ package com.keyinc.keymono.presentation.ui.theme
 
 import androidx.compose.ui.unit.dp
 
+internal val PaddingTiny = 4.dp
 internal val PaddingSmall = 8.dp
 internal val PaddingMedium = 16.dp
 internal val PaddingLarge = 32.dp
-internal val PaddingTiny = 4.dp
+
 internal val Padding24 = 24.dp
+internal val Padding128 = 128.dp
